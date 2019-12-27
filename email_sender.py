9db7433bb1ceb38a98224bd9c9540c4eba99e6c5
john jorjill <orgil9506@gmail.com>
2019-12-27 13:05:47 +0800

    more changes

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'John Jorjill'
 email['to'] = 'orgil9506@gmail.com'
 email['subject'] = 'Hello there!'
 
-email.set_content('Hello dear friend! Merry Christmas and happy new year!')
+email.set_content(html.substitute(name='John'),'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
